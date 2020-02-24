a2bdc2e75f71892acf0139fe1316ccb194731f91
FlorentCmb <florent.cmb77@gmail.com>
2020-02-24 10:30:21 +0100

    Cleaning src folder

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Librairies
+import React from 'react'
+import ReactDOM from 'react-dom'
+// Components
+import App from './App'
+// Styles
+import './index.css'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector('#root'))
+serviceWorker.register()
